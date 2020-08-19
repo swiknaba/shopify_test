616dc63dcd60137772cc66e28ed56fb121a6e7b7
swiknaba <swiknaba@users.noreply.github.com>
2020-08-20 00:57:06 +0200

    runs annotate

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: shops
+#
+#  id             :bigint           not null, primary key
+#  shopify_domain :string           not null
+#  shopify_token  :string           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+# Indexes
+#
+#  index_shops_on_shopify_domain  (shopify_domain) UNIQUE
+#
 class Shop < ActiveRecord::Base
   include ShopifyApp::ShopSessionStorage
 
