86faedb93c3a959fda805b6f4503cd2fc9e681b2
swiknaba <swiknaba@users.noreply.github.com>
2020-08-20 00:54:23 +0200

    more rubcop configs

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_08_14_091659) do
-
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "shops", force: :cascade do |t|
-    t.string "shopify_domain", null: false
-    t.string "shopify_token", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true
+  create_table 'shops', force: :cascade do |t|
+    t.string 'shopify_domain', null: false
+    t.string 'shopify_token', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['shopify_domain'], name: 'index_shops_on_shopify_domain', unique: true
   end
-
 end
